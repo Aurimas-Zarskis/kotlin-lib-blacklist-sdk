7d966ac0fae559b128bd2a77c1eefaca3d1a6cff
Aurimas Žarskis <a.zarskis@paysera.lt>
2020-06-18 10:50:14 +0300

    Don't create moshi adapter for filters

--- src/main/kotlin/com/paysera/lib/blacklist/entities/RestrictionFilter.kt
+++ src/main/kotlin/com/paysera/lib/blacklist/entities/RestrictionFilter.kt
@@ -1,12 +1,7 @@
 package com.paysera.lib.blacklist.entities
 
-import com.squareup.moshi.Json
-import com.squareup.moshi.JsonClass
-
-@JsonClass(generateAdapter = true)
 data class RestrictionFilter(
     val statuses: List<String>?,
     val hidden: Boolean?,
-    @Json(name = "user_id")
     val userId: Int
 )
